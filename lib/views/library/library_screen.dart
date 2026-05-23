f56ebe5465792713763259b9f6464bdace084454
Talitha <talithacandra140206@gmail.com>
2026-05-23 22:25:16 +0700

    update struktur dan file

--- lib/views/library/library_screen.dart
+++ lib/views/library/library_screen.dart
@@ -1,1 +1,17 @@
 // halaman riwayat cerita pengguna
+
+import 'package:flutter/material.dart';
+
+class LibraryScreen extends StatelessWidget {
+  const LibraryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Library',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
--- lib/views/search/search_screen.dart
+++ lib/views/search/search_screen.dart
@@ -1,1 +1,17 @@
 // halaman untuk fitur pencarian cerita
+
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Search',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
